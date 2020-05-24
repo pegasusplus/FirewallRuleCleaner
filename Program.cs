845cd58cb8f46d95e072bb47b2a406a7eaf2d3ee
Zhao Zhenping <pegasusplus@hotmail.com>
2020-05-24 18:22:24 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using NetFwTypeLib;
+using System;
+
+namespace FirewallRuleCleaner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShowAllowedApplications();
+        }
+
+        static void ShowAllowedApplications()
+        {
+            INetFwMgr netFwMgr = (INetFwMgr)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FwMgr"));
+
+            foreach(INetFwAuthorizedApplication app in netFwMgr.LocalPolicy.CurrentProfile.AuthorizedApplications)
+            {
+                Console.WriteLine("{0}, {1}, {2}, {3}", app.Name, app.ProcessImageFileName, app.RemoteAddresses, app.Scope);
+            }
+        }
+    }
+}
